82c972138ede78cac417f00a940ac894225eaf32
Frank Löppert <floeppert@dyn-10-18-6-217.office.noris.de>
2018-05-24 11:58:36 +0200

    added doku

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # ansible_mediawiki
 A sensible Modul to talk with a mediawiki API
+## Features
+* Supports BASIC Auth (Header) and MediaWiki Authentication
+* Request and Handle edit Tokens
+## Authentication
+* You can switch between BASIC Auth and MediaWiki Auth with the parameter 'login'
+* "login = False" is for BASIC Auth
+* "login = True" is for MediaWiki Auth
+## Examples
+### Example in Ansible
+* This example works with Basic Auth and creates a new site if the site not exists already.
+```yaml
+- name: Generating  Doku
+  hosts: localhost
+  gather_facts: False
+  tasks:
+    - name: Generating Rest Conenction String
+      set_fact:
+        rest_conn:
+          server: "server.fqdn"
+          username: "frank"
+          ssl_verify: False
+          login: False
+
+    - name: Check if Page exists
+      wiki_rest:
+        connection: "{{ rest_conn }}"
+        password: "{{ password }}"
+        method: get
+        path: "?action=query&prop=revisions&rvprop=content&formatversion=2&titles=Test_Page"
+      delegate_to: localhost
+      register: wiki_Test_page
+
+    - name: Create Test Page if not exists
+      wiki_rest:
+        connection: "{{ rest_conn }}"
+        password: "{{ password }}"
+        method: post
+        path: ""
+        payload:
+          action: edit
+          title: "Test_Page"
+          text: "{{ lookup('template', 'TestPage.j2') }}"
+      delegate_to: localhost
+      register: wiki_create_test_page
+      when: wiki_test_page.result.content.query.pages[0].pageid is not defined
+```
+### Example in Python
+* You can use this Module without Ansible directly in Pyhton
+```python
+import mediawiki
+import json
+username = "Admin@test"
+password = "<supersecurepassword>"
+
+# Create a new Instance
+restclient = mediawiki.wiki_rest_client("test.fqdn","/api.php","?action=query&meta=tokens&format=json",username,password,True,True,True)
+# Get Content of a Page
+get = restclient.call('get','?action=query&prop=revisions&rvprop=content&formatversion=2&titles=sandbox4','')
+if get.content:
+        json= json.loads(get.content.decode())
+print(json)
+print(restclient)
+
+# Create a new Page
+data = dict()
+data['action'] = "edit"
+data['text'] = '==Allgemein==\nHallo Welt!\nDies ist ein erster Test.'
+data['title'] = "Sandbox1"
+
+set = restclient.call('post','',data)
+print(set.json())
+```
---
+++ mediawiki.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python
+
+from ansible.module_utils.basic import AnsibleModule
+import ast
+import yaml
+import json
+import requests
+import os
+import sys
+from base64 import b64encode
+
+class rest_exception(Exception):
+    pass
+
+
+class wiki_rest_client():
+    def __init__(self, hostname, prefix, init_path, username, password, ssl_verify=True, token=True, login=False):
+        self.XSRFTOKEN='' # the actual token
+        self.client = requests.session()
+        self.hostname = hostname
+        self.prefix = prefix
+        userAndPass = "%s:%s" % (username, password)
+        if sys.version_info[0] < 3:
+            userAndPass = bytes(userAndPass) # python2
+        else:
+            userAndPass = bytes(userAndPass, 'UTF-8') # python3
+        userAndPass = b64encode(userAndPass).decode("ascii")
+        self.userAndPass = userAndPass
+        self.ssl_verify = ssl_verify
+        self.token = token
+        self.login = login
+        # if login = True, Login to mediawiki, else BASIC Auth as Header
+        if self.login:
+            # get login token
+            URL = "https://%s%s%s" % (self.hostname, self.prefix, '?action=query&meta=tokens&type=login&format=json')
+            response = self.client.get(URL, verify=self.ssl_verify)
+            if response.status_code != 200:
+                raise rest_exception(response.status_code, response.reason)
+            self.logintoken = response.json()["query"]["tokens"]["logintoken"]
+            # login
+            URL = "https://%s%s" % (self.hostname, self.prefix)
+            self.payload = dict()
+            self.payload["format"] = "json"
+            self.payload["action"] = "login"
+            self.payload["lgname"] = username
+            self.payload["lgpassword"] = password
+            self.payload["lgtoken"] = self.logintoken
+            response = self.client.post(URL, data=self.payload, verify=self.ssl_verify)
+            if response.status_code != 200:
+                raise rest_exception(response.status_code, response.reason)
+            if response.json()['login']['result'] != 'Success':
+                raise RuntimeError(response.json()['login']['reason'])
+        # edit token:
+        if self.token:
+            if self.login == False:
+                headers = { 'Authorization' : 'Basic %s' %  self.userAndPass }
+            else:
+                headers = ''
+            URL = "https://%s%s%s" % (self.hostname, self.prefix, init_path)
+            response = self.client.get(URL, headers=headers, verify=self.ssl_verify)
+            if response.status_code != 200:
+                raise rest_exception(response.status_code, response.reason)
+            self.XSRFTOKEN = response.json()["query"]["tokens"]["csrftoken"]
+
+    def call(self, method, path, payload):
+        if method == 'post':
+            method_function = self.client.post
+        else:
+            method_function = self.client.get
+        if self.login == False:
+            headers = {
+                'Authorization' : 'Basic %s' %  self.userAndPass,
+                }
+        else:
+            headers = ''
+        if self.token:
+            if method == 'post':
+                payload["token"] = self.XSRFTOKEN
+                payload['format'] = "json"
+                URL = "https://%s%s%s" % (self.hostname, self.prefix, path)
+            else:
+                URL = "https://%s%s%s%s" % (self.hostname, self.prefix, path, "&format=json")
+        response = method_function(URL, data=payload, headers=headers, verify=self.ssl_verify)
+        return response
+
+# ansible-facade
+class wiki_rest_facade(object):
+    def __init__(self, module):
+        self.module = module
+        self.connection = module.params['connection']
+        self.wiki_rest_client = wiki_rest_client(
+            self.connection.get('server', 'intra.qsu.office.noris.de'),
+            self.connection.get('rest_path', '/wiki/api.php'),
+            self.connection.get('token_path', '?action=query&meta=tokens&format=json'),
+            self.connection.get('username', ''),
+            module.params['password'],
+            ssl_verify = self.connection.get('ssl_verify', True),
+            token = self.connection.get('token', True),
+            login = self.connection.get('login', False),
+        )
+
+    def call(self):
+        payload = self.module.params['payload']
+        payload_obj = ast.literal_eval(payload)
+        response = self.wiki_rest_client.call(
+            self.module.params['method'],
+            self.module.params['path'],
+            payload_obj,
+        )
+        return response
+
+def main():
+    module = AnsibleModule(
+        argument_spec = dict(
+            connection = dict(type='dict', required=True),
+            password   = dict(required=True, no_log=True),
+            method     = dict(default='get', choices=['get', 'post']),
+            path       = dict(required=True),
+            payload    = dict(default=dict()),
+        )
+    )
+    client = wiki_rest_facade(module)
+    response = client.call()
+    result = dict(
+        status_code = response.status_code,
+        reason = response.reason,
+    )
+    if response.content:
+        result['content'] = json.loads(response.content.decode())
+    if result['content'].get('error'):
+        msg = result['content']['error']['code']
+        msg += ", "
+        msg += result['content']['error']['info']
+        module.fail_json(msg=msg)
+    module.exit_json(changed=True, result=result)
+
+if __name__ == '__main__':
+    main()
